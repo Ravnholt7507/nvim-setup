e75be3215e19ebc092a9e7da0f72d3fd6a6dc5dd
Andreas Jensen <Andreas.ravnholt@sky.uk>
2026-02-25 08:06:05 +0100

    update keybinds

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -2,7 +2,7 @@ local on_attach = function(_, bufnr)
     local opts = {buffer = bufnr}
     vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
     vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-    vim.keymap.set('n', 'gd', vim.lsp.buf.declaration, opts)
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
     vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
     vim.keymap.set('n', 'go', vim.lsp.buf.type_definition, opts)
     vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
--- nvim/lua/clangd.lua
+++ nvim/lua/clangd.lua
@@ -100,7 +100,7 @@ return {
   on_attach = function(client, bufnr)
     vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
     vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-    vim.keymap.set('n', 'gd', vim.lsp.buf.declaration, opts)
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
     vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
     vim.keymap.set('n', 'go', vim.lsp.buf.type_definition, opts)
     vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
